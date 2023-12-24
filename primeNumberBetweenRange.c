61a6730ec63ec50f3caba4a6c6b60b3764af7290
imTheRakib <mohammadshahariarh@gmail.com>
2023-12-25 00:22:47 +0600

    C files added

---
+++ primeNumberBetweenRange.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int start, end;
+
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+
+    int count2 = 0;
+
+    for (int i = start; i <= end; i++)
+    {
+        int count1 = 0;
+
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                count1++;
+                break;
+            }
+        }
+
+        if (i > 1)
+        {
+            if (count1 == 0)
+            {
+                printf("%d ", i);
+                count2++;
+            }
+        }
+    }
+    printf("\n\nTotal prime numbers between %d to %d = %d\n", start, end, count2);
+}
