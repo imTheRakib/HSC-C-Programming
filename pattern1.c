b042669899c961e57ddf9bd903b4d8b0fddb8395
imTheRakib <mohammadshahariarh@gmail.com>
2024-01-02 11:01:37 +0600

    C files added

---
+++ pattern1.c
@@ -0,0 +1,87 @@
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int row, col;
+
+//     for(row=1; row<=n; row++)
+//     {
+//         for(col=1; col<=row; col++)
+//         {
+//             printf("%d ", row);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// 1
+// 1 2
+// 1 2 3 
+// 1 2 3 4
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int row, col;
+
+//     for(row=1; row<=n; row++)
+//     {
+//         for(col=1; col<=row; col++)
+//         {
+//             printf("%d ", col);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// *
+// * *
+// * * *
+// * * * *
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int row, col;
+
+//     for(row=1; row<=n; row++)
+//     {
+//         for(col=1; col<=row; col++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+
+// * * * *
+// * * *
+// * * 
+// *
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int row, col;
+
+//     for(row=n; row>=1; row--)
+//     {
+//         for(col=1; col<=row; col++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
